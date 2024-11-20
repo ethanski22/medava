3bc2f95ddaf3d4943451011f08904415771333a0
Ethan Skowronski <ethanski22@icloud.com>
2024-11-20 17:05:15 -0500

    "completed" the project
    Worked through the read me but I have a very strong feeling that most of it is wrong

--- src/main/java/edu/uc/cs3003/medava/SupplyChain.java
+++ src/main/java/edu/uc/cs3003/medava/SupplyChain.java
@@ -3,5 +3,7 @@ package edu.uc.cs3003.medava;
 public class SupplyChain {
     public static void main(String args[]) {
         System.out.println("Hello there");
+        HospitalRunner.run();
+        Transporter semiTruck = new Transporter("LaserShip", 30.0, 80.0);
     }
 }
--- src/main/java/edu/uc/cs3003/medava/Transporter.java
+++ src/main/java/edu/uc/cs3003/medava/Transporter.java
@@ -1,3 +1,103 @@
 package edu.uc.cs3003.medava;
+
+import java.util.List;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+
+private List<Medicine> goods;
+
 public class Transporter {
+    private String mTransporterName;
+    public String getTransporterName() {
+        return mTransporterName;
+    }
+
+    private double mLowTemperature, mHighTemperature;
+
+    public Transporter(String transporterName, double lowTemp, double highTemp) {
+        mTransporterName = transporterName;
+        mLowTemperature = lowTemp;
+        mHighTemperature = highTemp;
+    }
+
+    public boolean load(Medicine itemToLoad) {
+        if (itemToLoad.isTemperatureRangeAcceptable(mLowTemperature, mHighTemperature)) {
+            System.out.println(String.format("Adding a %s to the transporter.", itemToLoad.getMedicineName()));
+            goods.add(itemToLoad);
+            return true;
+        }
+        return false;
+    }
+
+    // public boolean load(Object itemToLoad) {
+    //     if (itemToLoad.isTemperatureRangeAcceptable(mLowTemperature, mHighTemperature)) {
+    //         System.out.println(String.format("Adding a %s to the transporter.", itemToLoad.getMedicineName()));
+    //         goods.add(itemToLoad);
+    //         return true;
+    //     }
+    //     return false;
+    // }
+
+    public boolean load(Object itemToLoad) {
+        try {
+            Method isTemperatureRangeAcceptableMethod = itemToLoad.getClass().getMethod("isTemperatureRangeAcceptable",
+                    Double.class, Double.class);
+            boolean resultOfMethodCall = (boolean) isTemperatureRangeAcceptableMethod.invoke(itemToLoad,
+                    Double.valueOf(mLowTemperature), Double.valueOf(mHighTemperature));
+            if (resultOfMethodCall) {
+                goods.add(itemToLoad);
+            }
+            return resultOfMethodCall;
+        } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException
+                | InvocationTargetException e) {
+            return false;
+        }
+    }
+
+    public Medicine unload() {
+        return goods.remove(0);
+    }
+
+    public Object unload() {
+        return goods.remove(0);
+    }
+
+    public boolean isEmpty() {
+        return goods.isEmpty();
+    }
+```
+}
+
+private List<Medicine> goods;
+
+{
+    goods = new List<Medicine>();
+  }
+
+  {
+    goods = new ArrayList<Medicine>();
+  }
+
+  public List<Medicine> goods;
+
+  public void ship() {
+    // Do some shipping!
+  }
+  private List<Object> goods;
+  goods = new ArrayList<Object>();
+
+  private List<Shippable> goods;
+  goods = new ArrayList<Shippable>();
+
+  public Shippable unload() {
+    return goods.remove(0);
+}
+
+public boolean load(Shippable itemToLoad) {
+    if (itemToLoad.isTemperatureRangeAcceptable(mLowTemperature, mHighTemperature)) {
+        return goods.add(itemToLoad);
+    }
+    return false;
 }
